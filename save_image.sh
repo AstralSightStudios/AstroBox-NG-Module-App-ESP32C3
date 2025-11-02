46734fbb3e2fee4c33353725f9e544a1a6abd3ac
Searchstars <searchstars@163.com>
2025-11-02 21:10:29 +0800

    修改一些脚本

--- save_image.sh
+++ save_image.sh
@@ -1,1 +1,1 @@
-espflash save-image --chip esp32c3 ../../target/riscv32imc-esp-espidf/debug/app_esp32c3 ./firmware.bin
+espflash save-image --chip esp32c3 ./target/riscv32imc-esp-espidf/debug/app_esp32c3 ./firmware.bin
